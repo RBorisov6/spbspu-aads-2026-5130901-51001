aea65df0522ffc841c86285a5a0669b3791b399c
RBorisov6 <borisovruslan1512@gmail.com>
2026-06-22 18:19:10 +0300

    S3: rewrite readGraphs using token-based parsing

--- borisov.ruslan/S3/io.cpp
+++ borisov.ruslan/S3/io.cpp
@@ -1,53 +1,35 @@
 #include "io.hpp"
-#include <iostream>
-#include <sstream>
+#include <istream>
 #include <string>
 
 namespace borisov
 {
   bool readGraphs(std::istream& in, GraphTable& graphs)
   {
-    std::string line;
-    while (std::getline(in, line))
+    std::string graphName;
+    while (in >> graphName)
     {
-      if (line.empty())
-      {
-        continue;
-      }
-      std::istringstream header(line);
-      std::string graphName;
       std::size_t edgeCount = 0;
-      if (!(header >> graphName >> edgeCount))
+      if (!(in >> edgeCount))
       {
         return false;
       }
-
-      Graph g;
-      for (std::size_t i = 0; i < edgeCount; ++i)
-      {
-        std::string edgeLine;
-        while (std::getline(in, edgeLine))
-        {
-          if (!edgeLine.empty())
-          {
-            break;
-          }
-        }
-        std::istringstream edgeStream(edgeLine);
-        std::string from, to;
-        unsigned int weight = 0;
-        if (!(edgeStream >> from >> to >> weight))
-        {
-          return false;
-        }
-        g.addEdge(from, to, weight);
-      }
-
       if (graphs.size() >= graphs.slots())
       {
         graphs.rehash(graphs.slots() * 2 + 1);
       }
-      graphs.add(graphName, g);
+      graphs.add(graphName, Graph());
+      Graph& g = graphs.at(graphName);
+      for (std::size_t i = 0; i < edgeCount; ++i)
+      {
+        std::string from, to;
+        std::size_t weight = 0;
+        if (!(in >> from) || !(in >> to) || !(in >> weight))
+        {
+          return false;
+        }
+        g.addEdge(from, to, static_cast<unsigned int>(weight));
+      }
     }
     return true;
   }
